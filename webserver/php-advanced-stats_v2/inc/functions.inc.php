c14b97bcae46ab34aa86e538597a9414f0cd4379
Zipcore <zipcore@googlemail.com>
2014-11-25 00:02:58 +0100

    Small changes

--- webserver/php-advanced-stats_v2/inc/functions.inc.php
+++ webserver/php-advanced-stats_v2/inc/functions.inc.php
@@ -2,23 +2,20 @@
 	include "config.inc.php";
 	
 	$link = mysqli_connect($serverip, $dbusername, $dbpassword, $dbname) or die("Couldn't make connection.");
-	
 	mysqli_set_charset($link, "utf8");
 
 	function steam2friend($steam_id)
 	{
 		$steam_id=strtolower($steam_id);
-		if(substr($steam_id,0,7)=='steam_0'){
+		if (substr($steam_id,0,7)=='steam_0')
+		{
 			$tmp=explode(':',$steam_id);
-			if((count($tmp)==3) && is_numeric($tmp[1]) && is_numeric($tmp[2])){
+			if ((count($tmp)==3) && is_numeric($tmp[1]) && is_numeric($tmp[2]))
 				return bcadd((($tmp[2]*2)+$tmp[1]),'76561197960265728');
-			} else {
-				return false;
-			}
-		} else {
-			return false;
+			else return false;
 		}
-	}
+		else return false;
+	} 
 	
 	function unitFormat($number, $decimals = 2)
 	{
@@ -33,9 +30,7 @@
 		// If decimals is not numeric or decimals is less than 0 
 		// then set default value
 		if (!is_numeric($decimals) || $decimals < 0) 
-		{
 			$decimals = 2;
-		}
 
 		// Format output
 		return sprintf('%.' . $decimals . 'f'.$unit, $value);
