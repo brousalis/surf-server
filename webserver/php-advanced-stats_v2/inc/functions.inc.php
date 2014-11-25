97d41b16693b2d1885b655b331fdbcb881a0fca6
TheZyppi <nico@kamphenkel.net>
2014-11-25 11:09:46 +0100

    Fix for the steam2friend Function

--- webserver/php-advanced-stats_v2/inc/functions.inc.php
+++ webserver/php-advanced-stats_v2/inc/functions.inc.php
@@ -7,15 +7,18 @@
 	function steam2friend($steam_id)
 	{
 		$steam_id=strtolower($steam_id);
-		if (substr($steam_id,0,7)=='steam_0')
-		{
-			$tmp=explode(':',$steam_id);
-			if ((count($tmp)==3) && is_numeric($tmp[1]) && is_numeric($tmp[2]))
+		$tmp=explode(':',$steam_id);
+		if(substr($tmp[0],0,-1)=='steam_'){
+			
+			if((count($tmp)==3) && is_numeric($tmp[1]) && is_numeric($tmp[2])){
 				return bcadd((($tmp[2]*2)+$tmp[1]),'76561197960265728');
-			else return false;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
 		}
-		else return false;
-	} 
+	}
 	
 	function unitFormat($number, $decimals = 2)
 	{
